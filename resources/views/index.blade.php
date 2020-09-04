bef679df412a610463b8d27c8c68c3dfa4cd14f8
Arman85 <advanced315@gmail.com>
2020-09-04 20:29:01 +0600

    finish

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -2,25 +2,27 @@
 
 @section('content')
 
-    <div class="container">
-        <div class="row">
-            <div class="col-sm-12">
-                <form action="" class="mt-5">
-                    <div class="row d-flex justify-content-center">
-                        <div class="col-md-6">
-                            <div class="form-group">
-                                <select name="" id="category" class="form-control mb-4">
-                                    @foreach($categories as $category)
-                                        <option value="{{ $category->id }}">{{ $category->name }}</option>
-                                    @endforeach
-                                </select>
-                            </div>
-                            <div class="form-group d-flex justify-content-center">
-                                <button class="btn btn-lg btn-primary category-btn" type="button">Выбрать</button>
+    <div class="wrapper">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-12">
+                    <form action="" class="mt-5">
+                        <div class="row d-flex justify-content-center">
+                            <div class="col-md-6">
+                                <div class="form-group">
+                                    <select name="" id="category" class="form-control mb-4">
+                                        @foreach($categories as $category)
+                                            <option value="{{ $category->id }}">{{ $category->name }}</option>
+                                        @endforeach
+                                    </select>
+                                </div>
+                                <div class="form-group d-flex justify-content-center">
+                                    <button class="btn btn-lg btn-primary category-btn" type="button">Выбрать</button>
+                                </div>
                             </div>
                         </div>
-                    </div>
-                </form>
+                    </form>
+                </div>
             </div>
         </div>
     </div>
